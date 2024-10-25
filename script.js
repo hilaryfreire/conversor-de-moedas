8d307a4ebc7dc1d81618f4eeb1e3e5ebffede1da
Hilary Freire <hilary.caroline@alunoescolassociais.org.br>
2024-10-25 08:55:16 -0300

    iniciando o Readme

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     event.preventDefault();
 
     // Obter valores de entrada informados pelo usuário
-    const valor = (document.getElementById
-    ('container').value);
+    const valor = (document.getElementById('value').value);
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
 
     // Definir taxas de câmbio fixas
     const exchangeRates = {
-        USD: { BR:  5.70, EUR: 0.93 },
+        USD: { BRL:  5.70, EUR: 0.93 },
         BRL: { USD: 0.18, EUR: 0.16 },
-        EUR: { USD: 1.08, BRL: 6.16 },
+        EUR: { USD: 1.08, BRL: 6.16 }
     };
+    // Conversão símples de moedas 
+    let valorConvertido;
+    if(daMoeda === paraMoeda){
+        valorConvertido = valor;
+    }else{
+        valorConvertido = valor * exchangeRates [daMoeda][paraMoeda];
+    }
+
+    const conversao = document.getElementById('conversao');
+    conversao.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda}`;
 });
 
